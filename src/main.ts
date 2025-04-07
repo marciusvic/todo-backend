12792a745c0809e54832145e70115bb9f0edd2f2
Marcio Victor <marcio9769@gmail.com>
2025-04-07 19:38:23 -0300

    fix: enable cors to localhost frontend

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,13 @@ import * as dotenv from 'dotenv';
 async function bootstrap() {
   dotenv.config();
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors({
+    origin: ['http://localhost:5173'],
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });
+
   await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
