cfba5b06c7e2ec22b9a409cb596832750dd0a487
Marcio Victor <marcio9769@gmail.com>
2025-04-07 23:57:39 -0300

    fix: allow CORS to vercel app

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: [
-      'http://localhost:5173',
-      'https://todolist-alpha-cyan.vercel.app/',
-    ],
+    origin: ['http://localhost:5173', 'https://todolist-alpha-cyan.vercel.app'],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
