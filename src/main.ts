44a34b05189f53b3be1cb714609b9f01e42abcbb
Marcio Victor <marcio9769@gmail.com>
2025-04-07 23:49:47 -0300

    fix: allow vercel font

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,10 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   app.enableCors({
-    origin: ['http://localhost:5173'],
+    origin: [
+      'http://localhost:5173',
+      'https://todolist-alpha-cyan.vercel.app/',
+    ],
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
